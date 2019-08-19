c93cadd68f8a576736ce1baf625d724646bb6472
Nathan Krasney <natankrasney@gmail.com>
2019-08-19 14:16:32 +0300

    hello express

---
+++ hello_express.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+console.log(`server listens on port : ${PORT}`);
+
+app.get("/", (req, res) => {
+  res.send("<h1>This is Home page</h1>");
+});
+
+app.get("/help", (req, res) => {
+  res.send("<h1>This is Help page</h1>");
+});
+
+app.get("/support", (req, res) => {
+  res.send("<h1>This is Support page</h1>");
+});
+
+app.listen(PORT);

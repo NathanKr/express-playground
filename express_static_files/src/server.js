d7dcfb9f06421ee6dc2501b1edb7625977f98b05
Nathan Krasney <natankrasney@gmail.com>
2019-08-30 11:33:58 +0300

    small change

--- express_static_files/src/server.js
+++ express_static_files/src/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, "..", "public");
 // loads index.html when route is /
 // loads help.html when route is /help.html
 // loads support.html when route is /support.html
-app.use( (publicPath));
+app.use( express.static(publicPath));
 
 app.listen(PORT, () => {
   console.log(`server listens on port : ${PORT}`);

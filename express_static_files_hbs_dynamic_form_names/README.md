2d97e9b8aee3afd049453cc9f7a34e1bb0f55444
Nathan Krasney <natankrasney@gmail.com>
2020-10-25 10:50:19 +0200

    initial version

---
+++ express_static_files_hbs_dynamic_form_names/README.md
@@ -0,0 +1,6 @@
+<h2>More server side todo</h2>
+<ul>
+    <li>validation</li>
+    <li>delete</li>
+    <li>update</li>
+</ul>
--- express_static_files_hbs_dynamic_form_names/src/server.js
+++ express_static_files_hbs_dynamic_form_names/src/server.js
@@ -3,16 +3,27 @@ const path = require("path"),
   app = express(),
   PORT = 8080;
 const publicPath = path.join(__dirname, "..", "public");
-const names = ["Nathan" , "Nitzan" , "Yael" , "Jim"]
+let names = []
 
 app.set("view engine", "hbs");
 
 // --- serve static html\css\js\image files
 app.use(express.static(publicPath));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 app.get('/names',(req,res) =>{
   // --- serve help.hbs from views directory
+  res.render("names",{namesLen: names.length , names : names});
+})
+
+app.post('/addName',(req,res) =>{
+  const formData = req.body;
+  const name = formData.personName;
+  names.push(name);
+
   res.render("names",{namesLen: names.length , names : names});
 })
 

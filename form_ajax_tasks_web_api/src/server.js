cbd7cf00204ece7ae2d7d100b8de0432e1313fee
Nathan Krasney Laptop <natankrasney@gmail.com>
2021-10-26 08:24:56 +0300

    create new task

--- form_ajax_tasks_web_api/src/server.js
+++ form_ajax_tasks_web_api/src/server.js
@@ -4,13 +4,26 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const tasks = [
-  { id: 1, name: "task1", completed: true },
-  { id: 2, name: "task2", completed: false },
-];
+// { id: 1, name: "task1", completed: true },
+let freeId = 0;
+let tasks = [];
 
 const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
+app.use(express.json());
+
+app.post("/tasks", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    const newTask = {name,id:freeId,completed:false};
+    freeId++;
+    tasks.push(newTask);
+    // send new object so client can use it without need to use get tasks
+    res.status(201).send(newTask); 
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 app.get("/tasks", (req, res) => {
   res.send(tasks);
